302e80a7879b897c4475708ebd7c920cd4525f03
bigic <brani.igic@gmail.com>
2016-07-31 17:31:41 +1000

    added scrolling home nav bar

--- _site/js/simple.js
+++ _site/js/simple.js
@@ -4,4 +4,22 @@ $(function(){
     $(this).siblings().toggleClass("show-hide", this.checked)
   }).change();
 	
+
+	function scrollingnavmenu() {
+		var thinheaderimage = document.getElementById('thin-top-image'); 
+		var thinheaderimageheight = thinheaderimage.clientHeight;
+		
+		$(window).scroll(function () {
+			if ($(window).scrollTop() > thinheaderimageheight) {
+				$(".post-hometab").addClass("post-hometab-fixed");
+			}
+			if ($(window).scrollTop() < thinheaderimageheight + 1) {
+				$(".post-hometab").removeClass("post-hometab-fixed");
+			}
+		});
+	};
+	
+	scrollingnavmenu();
+	$(window).resize(scrollingnavmenu);
+	
 });
